605489e570223e287065c2229a1aa350e569e1e0
gusmorini <gustavommh@gmail.com>
2018-09-04 08:44:37 -0300

    erro 404 view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('home.templates.index')
+
+@section('conteudo')
+
+<!-- <div class="title-pg">
+	<h1 class="title-pg">Erro 404 Página não encontrada</h1>
+</div> -->
+
+<div class="erro">
+	
+	<img src="imgs/404.jpg" title="erro 404">
+	<h3>Página não encontrada</h3>
+
+</div>
+
+
+@endsection
